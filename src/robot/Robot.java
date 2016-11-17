db17e5f1e30c669e1977860dff54f190813fd28e
Wendong Yang <wendong.yang@yahoo.com>
2016-11-17 12:36:00 -0600

    logging

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Stack;
+import java.util.logging.Logger;
 
 import cleanManager.CleanManager;
 import floorPlanManager.Coordinate;
@@ -14,6 +15,8 @@ import powermanagement.VacuumManager;
 import sensors.SensorAPI;
 
 public class Robot {
+	private static final Logger LOGGER = Logger.getLogger(Robot.class.getName());
+
 	private FloorPlanManager fpm = FloorPlanManager.getInstance();
 	private Coordinate currentLocation;
 	private String chargeStation = "0,0";
@@ -50,8 +53,10 @@ public class Robot {
 
 			fpm.setCurrentLocation(currentLocation.getX(), currentLocation.getY());
 			if (!visited.contains(nextStep)) {
+				LOGGER.info("---------------------------");
 				System.out.println("---------------------------");
 				if (!enoughPower()) {
+					LOGGER.info("not enough Power going back to station" + "----remaining power: " + pwm.getRemainingBattery());
 					System.out.println("not enough Power going back to station" + "----remaining power: " + pwm.getRemainingBattery());
 
 					LinkedList<Vertex> path = fpm.getPath(currentLocation.getStringXY(), chargeStation);
@@ -59,10 +64,12 @@ public class Robot {
 					for (Vertex ver : path) {
 						
 						tempNext = ver.getName();
+						LOGGER.info("Cleaner moving to: " + ver);
 						System.out.println("Cleaner moving to: " + ver);
 						pwm.consumeBattery((int) fpm.getCostPath(current, tempNext));
 						current = ver.getName();
 						currentLocation.setString(current);
+						LOGGER.info("Cleaner now at: " + currentLocation.getStringXY());
 						System.out.println("Cleaner now at: " + currentLocation.getStringXY());
 
 					}
@@ -71,8 +78,11 @@ public class Robot {
 				}
 
 				// System.out.println(fpm.getCostPath(current, nextStep));;
+				
 				System.out.println("Battery State: " + pwm.getRemainingBattery());
+				LOGGER.info("Battery State: " + pwm.getRemainingBattery());
 				System.out.println("Cleaner now at: " + current);
+				LOGGER.info("Cleaner now at: " + current);
 				// System.out.println("going from " + current + " to " +
 				// nextStep);
 				LinkedList<Vertex> paths = fpm.getPath(current, nextStep);
@@ -82,10 +92,12 @@ public class Robot {
 					for (Vertex ver : paths) {
 						tempNext = ver.getName();
 						System.out.println("Cleaner moving to: " + ver);
+						LOGGER.info("Cleaner moving to: " + ver);
 						pwm.consumeBattery((int) fpm.getCostPath(current, tempNext));
 						current = ver.getName();
 						currentLocation.setString(current);
 						System.out.println("Cleaner now at: " + currentLocation.getStringXY());
+						LOGGER.info("Cleaner now at: " + currentLocation.getStringXY());
 
 					}
 				}
@@ -94,9 +106,12 @@ public class Robot {
 					clm.removeLoad();
 					pwm.consumeBattery(fpm.getSurfaceType(currentLocation));
 					System.out.println("cleaning at " + nextStep);
+					LOGGER.info("cleaning at " + nextStep);
 					fpm.setDirt(currentLocation, 1);
 					System.out.println("All Cleaned: " + clm.isAllCleaned());
+					LOGGER.info("All Cleaned: " + clm.isAllCleaned());
 					System.out.println("---------------------------");
+					LOGGER.info("---------------------------");
 				}
 				// System.out.println("I am at " +
 				// fpm.getCurrentLocation().getStringXY());
@@ -115,6 +130,7 @@ public class Robot {
 
 		if (powerLevel >= fpm.getCostPath(currentLocation.getStringXY(), chargeStation)) {
 			System.out.println("enough power");
+			LOGGER.info("enough power");
 			return true;
 		}
 		return false;
